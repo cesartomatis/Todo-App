ef29373727552e9125f939cdc6613d493b023a81
Cesar Tomatis <cesartomatis@outlook.com>
2021-02-10 11:34:53 +0100

    added store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,9 @@
-import { createStore } from "vuex";
+import { createLogger, createStore } from 'vuex';
 
 export default createStore({
+  plugins: process.env.NODE_ENV === 'development' ? [createLogger()] : [],
   state: {},
   mutations: {},
   actions: {},
-  modules: {}
+  modules: {},
 });
